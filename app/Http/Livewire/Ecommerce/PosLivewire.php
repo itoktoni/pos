ab4b77e42f76c77356cacfa3f67a4d5ab295f678
Itok toni laksono <itok.toni@gmail.com>
2021-03-18 09:32:03 +0700

    update

--- app/Http/Livewire/Ecommerce/PosLivewire.php
+++ app/Http/Livewire/Ecommerce/PosLivewire.php
@@ -121,7 +121,7 @@ class PosLivewire extends Component
     public function updateProduct()
     {
         $detail = new ProductDetail();
-        $query = ProductFacades::dataRepository();
+        $query = ProductFacades::dataRepository()->leftJoin($detail->getTable(), 'item_detail_product_id', ProductFacades::getKeyName())->where('item_detail_branch_id', auth()->user()->branch);
 
         if (!empty($this->search)) {
             $query->where('item_product_name', 'like', "%$this->search%");
@@ -277,10 +277,11 @@ class PosLivewire extends Component
                 $check_item = OrderDetailFacades::saveRepository($product);
 
 
-                $item_detail = ProductDetail::where('item_detail_product_id', $item->id)->where('item_detail_branch_id', auth()->user()->branch)->get();
-                if($item_detail->count() > 0){
+                $item_detail = ProductDetail::where('item_detail_product_id', $item->id)
+                ->where('item_detail_branch_id', auth()->user()->branch);
+                if($item_detail->get()->count() > 0){
 
-                    $total_qty = $item_detail->sum('item_detail_stock_qty');
+                    $total_qty = $item_detail->get()->sum('item_detail_stock_qty');
                     $selisih = $total_qty - $item->quantity;
                     
                     $item_detail->delete();
